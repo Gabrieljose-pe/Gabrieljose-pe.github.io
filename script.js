bae6d32c8efad3780e250e73ab08b5fd4162a379
Gabriel José <164208289+Gabrieljose-pe@users.noreply.github.com>
2024-03-20 12:34:20 -0300

    upload site Meu Portfólio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('formulario').addEventListener('submit', function(evento) {
+    evento.preventDefault(); // vazio, só pra fazer o alert
+
+    alert('Formulário enviado com sucesso!'); 
+});
